fec369d7af5353a590d8ae370f0f2ff11e6bd11b
zakiya <zakiya@chapterthree.com>
2021-03-31 12:12:45 -0700

    Allow custom aliases on transaction nodes.

--- cypress/integration/_sfgov_redirect.js
+++ cypress/integration/_sfgov_redirect.js
@@ -1,24 +1,30 @@
 import titleText from "./titleText";
 import inputElements from "./inputElements";
 import createTopicNode from "./topic";
+const op = { force: true };
 
-let createTransactionNode = (title) => {
-  cy.get(inputElements.textfieldTitle).type(title, { force: true });
+let createTransactionNode = (title, customTranslatedAlias) => {
+  cy.get(inputElements.textfieldTitle).type(title, op);
+
+  if (customTranslatedAlias === true) {
+    cy.get(inputElements.inputAliasCheckbox).uncheck({ force: true });
+    cy.get(inputElements.textfieldAlias).type("-custom-alias", op);
+  }
+
   cy.get(inputElements.selectPublish).select("published");
   cy.get(inputElements.buttonSubmit).click({
     force: true,
   });
 };
 
-let createTransaction = (title, translation) => {
+let createTransaction = (title, translation, customTranslatedAlias) => {
   cy.visit("/node/add/transaction");
   createTransactionNode(title);
 
   if (translation === true) {
     cy.get(inputElements.aTranslateTab).click({ force: true });
     cy.get('[href$="/translations/add/en/es"]').click();
-
-    createTransactionNode(title + "-- es");
+    createTransactionNode(title + "-- es", customTranslatedAlias);
   }
 };
 
@@ -38,9 +44,9 @@ let login = () => {
 describe("sf.gov", () => {
   it("Test", () => {
     login();
-    createTransaction(titleText("O", "A"));
     createTransaction(titleText("O", "B"), true);
     createTransaction(titleText("D", "B"), true);
     createTopicNode("Topic");
+    createTransaction(titleText("O", "A"), true, true);
   });
 });
--- cypress/integration/inputElements.js
+++ cypress/integration/inputElements.js
@@ -10,6 +10,9 @@ const inputElements = {
     '[data-drupal-selector="edit-field-department-services-0-subform-field-dept-service-sect-services-0-target-id"]',
   serviceSectionAutocomplete2:
     '[name="field_department_services[0][subform][field_dept_service_sect_services][1][target_id]"]',
+
+  inputAliasCheckbox: '[name="path[0][pathauto]"]',
+  textfieldAlias: '[name="path[0][alias]"]',
   selectPublish: '[data-drupal-selector="edit-moderation-state-0-state"]',
   buttonSubmit: '[data-drupal-selector="edit-submit"]',
   aTranslateTab: '.sfgov-tabbed-navigation [href$="translations"]',
