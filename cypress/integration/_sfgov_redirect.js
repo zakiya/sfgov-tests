80a5d0682cf7711f33f2b081b34a9d352fdd63b2
zakiya <zakiya@chapterthree.com>
2021-04-01 10:34:22 -0700

    Proper env declaration.

--- cypress/integration/_sfgov_redirect.js
+++ cypress/integration/_sfgov_redirect.js
@@ -10,8 +10,8 @@ let login = () => {
     url: "/user/login",
     form: true,
     body: {
-      name: Cypress.env.un,
-      pass: Cypress.env.pw,
+      name: Cypress.env("un"),
+      pass: Cypress.env("pw"),
       form_id: "user_login_form",
     },
   });
